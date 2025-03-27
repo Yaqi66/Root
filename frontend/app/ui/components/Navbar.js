37a510af1b5d91c1f9779722a771732b1bb47860
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2025-03-27 16:13:35 +0000

    updated ui

--- frontend/app/ui/components/Navbar.js
+++ frontend/app/ui/components/Navbar.js
@@ -1,16 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/page.module.css';
 
 export default function Navbar() {
     return (
         <nav className={styles.navBar}>
             <div className={styles.navLogo}>
-                <Image
-                    src={'/pictures/logoLight.png'}
-                    alt="Logo"
-                    width={200}
-                    height={80}
-                />
+                <Link href="/">
+                    <Image
+                        src={'/pictures/logoAlternative.png'}
+                        alt="Logo"
+                        width={90}
+                        height={80}
+                        style={{ cursor: 'pointer' }}
+                    />
+                </Link>
             </div>
             {/* Optional dock or buttons if needed */}
             {/* <div className={styles.navDock}>Dock</div> */}
